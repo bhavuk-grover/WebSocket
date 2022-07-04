572b98738098e77e97705e8c8aa6739762b398cd
bhavuk-grover <bhavuk.grover@sentieo.com>
2022-07-04 12:50:44 +0530

    WebSocket

---
+++ main.py
@@ -0,0 +1,25 @@
+import json
+import asyncio
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi import WebSocket
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+temp = Jinja2Templates(directory="templates")
+
+with open("csvjson.json" , 'r') as file:
+    stockdata = iter(json.loads(file.read()))
+
+@app.get("/")
+def read(request: Request):
+    return temp.TemplateResponse("index.html", {"request": request})
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        await asyncio.sleep(2)
+        data = next(stockdata)
+        await websocket.send_json(data)
+
